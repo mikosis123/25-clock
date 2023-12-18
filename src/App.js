9aec19c383b3b13e71732040cbebd143dad8c794
mikosis123 <mikosis93@gmail.com>
2023-12-18 11:39:29 -0800

    fuctional page creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,113 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Display from "./Display";
+import TimeSetter from "./TimeSetter";
 
-import './App.css';
+const defaultBreakTime = 5 * 60;
+const defaultSessionTime = 25 * 60;
+const min = 60;
+const max = 60 * 60;
+const interval = 60;
+
 
 function App() {
+  const [breakTime, setBreakTime] = useState(defaultBreakTime);
+  const [sessionTime, setSessionTime] = useState(defaultSessionTime);
+  const [displayState, setDisplayState] = useState({
+    time: sessionTime,
+    timeType: "Session",
+    timerRunning: false,
+  });
+
+  useEffect(() => {
+    let timerID=""
+    if (!displayState.timerRunning) return;
+
+    if (displayState.timerRunning) {
+      timerID = window.setInterval(decrementDisplay, 1000);
+    }
+
+    return () => {
+      window.clearInterval(timerID);
+    };
+  }, [displayState.timerRunning]);
+
+  useEffect(() => {
+    if (displayState.time === 0) {
+      setDisplayState((prev) => ({
+        ...prev,
+        timeType: prev.timeType === "Session" ? "Break" : "Session",
+        time: prev.timeType === "Session" ? breakTime : sessionTime,
+      }));
+    }
+  }, [displayState, breakTime, sessionTime]);
+
+  const reset = () => {
+    setBreakTime(defaultBreakTime);
+    setSessionTime(defaultSessionTime);
+    setDisplayState({
+      time: defaultSessionTime,
+      timeType: "Session",
+      timerRunning: false,
+    });
+   
+  };
+
+  const startStop = () => {
+    setDisplayState((prev) => ({
+      ...prev,
+      timerRunning: !prev.timerRunning,
+    }));
+  };
+
+  // const changeBreakTime = (time) => {
+  //   if (displayState.timerRunning) return;
+  //   setBreakTime(time);
+  // };
+
+  const decrementDisplay = () => {
+    setDisplayState((prev) => ({
+      ...prev,
+      time: prev.time - 1,
+    }));
+  };
+
+  const changeSessionTime = (time) => {
+    if (displayState.timerRunning) return;
+    setSessionTime(time);
+    setDisplayState({
+      time: time,
+      timeType: "Session",
+      timerRunning: false,
+    });
+  };
+
   return (
-    <div className="App">
-      
+    <div className="clock">
+      <div className="setters">
+        <div className="break">
+          <h4 id="break-label">Break Length</h4>
+          
+        </div>
+        <div className="session">
+          <h4 id="session-label">Session Length</h4>
+          <TimeSetter
+            time={sessionTime}
+            setTime={changeSessionTime}
+            min={min}
+            max={max}
+            interval={interval}
+            type="session"
+          />
+        </div>
+      </div>
+      <Display
+        displayState={displayState}
+        reset={reset}
+        startStop={startStop}
+      />
+
+     
     </div>
   );
 }
